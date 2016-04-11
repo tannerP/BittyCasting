1bc8308b0af98af237ada2e14bb3a26d6c37baf3
Tanner <tannerphan@gmail.com>
2016-04-11 15:53:20 -0700

    https

--- server.js
+++ server.js
@@ -72,12 +72,12 @@ app.use(express.static(__dirname + '/public'));
   app.all('*', function(req, res, next){
     res.sendFile(path.join(__dirname+"/public/app/views/index.html"))
   })
-  app.listen(config.port);
+/*  app.listen(config.port);*/
   
-/*var server = https.createServer(options, app);
+var server = https.createServer(options, app);
     server.listen(config.port, function(){
       console.log(config.port);
-    });*/
+    });
 
   /*console.log(server);*/
 /*});*/
