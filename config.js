0181603bb546da06666f0f4fa075e42483aecfe0
Tanner <tannerphan@gmail.com>
2016-05-10 15:45:00 -0700

    Added logo from S3

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.dev = {
 	secret: "Hello 2016",
   "USER"    : "",           
   "PASS"    : "",
-  "HOST"    : "ec2-54-191-157-140.us-west-2.compute.amazonaws.com",  
+  "HOST"    : "127.0.0.1",  
   "PORT"    : "27017", 
   "DATABASE" : "prod",
   "api_key"	: "key-22cf943037f7135a79633e5f3fb255e1",
