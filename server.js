2f7922f5b459453b669d5d465361da3b5f1003d0
Tanner <tannerphan@gmail.com>
2016-05-22 12:27:55 -0700

    Added env variable

--- server.js
+++ server.js
@@ -16,77 +16,95 @@ var Role = require('./server/models/role');
 var Project = require('./server/models/project');
 var Applicant = require('./server/models/applicant');
 var express = require('express'); //EXPRESS Package
-var app = express();	//define our app using express
-var bodyParser = require('body-parser');// get body-parser
+var app = express(); //define our app using express
+var bodyParser = require('body-parser'); // get body-parser
 var morgan = require('morgan'); //use to see requests
 var mongoose = require('mongoose') //for working with mongoDB
 var path = require('path');
 var User = require(__dirname + '/server//models/user.js');
-var config = require('./config').prod; //get config file
+
 var extend = require("extend");
 app.use(require('prerender-node').set('prerenderToken', 'QyDUvf8RhPXGiwzgHUS4'));
 
 
 app.use(require('prerender-node')
-    .set('prerenderToken', 'QyDUvf8RhPXGiwzgHUS4'));
- 
+  .set('prerenderToken', 'QyDUvf8RhPXGiwzgHUS4'));
+
+
+var env = process.argv[2];
+  
+  console.log(env)
+switch (env) {
+  case "prod":
+    var config = require('./config').prod; //get config file
+    break;
+  case "dev":
+    var config = require('./config').dev; //get config file
+    app.use(morgan('dev')); //HTTP logger
+    break;
+  default:
+    var config = require('./config').prod; //get config file
+    break;
+
+}
+
 /*var io = require('socket.io')(app);*/
 
 //var port = config.port; //PORT
-app.use(morgan('dev')); //HTTP logger
 
 //==================================--APP--====================================
-app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 //configure app to handle CORS requests
-app.use(function(req,res,next){
-	res.setHeader('Access-Control-Allow-Orgin','*');
-	res.setHeader('Access-Control-Allow-Method','GET,POST');
-	res.setHeader('Access-Control-Allow-Headers',
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Orgin', '*');
+  res.setHeader('Access-Control-Allow-Method', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers',
     'X-Request-With,content-type,\Authorization');
-/*  res.setHeader('Last-Modified', (new Date()).toUTCString());*/
+  /*  res.setHeader('Last-Modified', (new Date()).toUTCString());*/
   /*res.setHeader('Cache-Control', 'public, max-age=3155'); */
-	next();
+  next();
 });
 
 //==================================--DB--====================================
 
-var dbPath  = "mongodb://" +
-    config.HOST + ":"+
-    config.PORT + "/"+	
-    config.DATABASE;
+var dbPath = "mongodb://" +
+  config.HOST + ":" +
+  config.PORT + "/" +
+  config.DATABASE;
 /*console.log(dbPath);*/
 /*mongoose.connect('mongodb://localhost/local');*/
 mongoose.connect(dbPath);
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
+db.once('open', function(callback) {
   console.log('MONGO: successfully connected to db');
 });
 
-var apiRoutes = require(__dirname + '/server/routes/api')(app,express);
-var publicRoutes = require(__dirname + '/server/routes/public')(app,express);
+var apiRoutes = require(__dirname + '/server/routes/api')(app, express);
+var publicRoutes = require(__dirname + '/server/routes/public')(app, express);
 
 app.use(express.static(__dirname + '/public'));
 /* S3 Config*/
 
-app.use('/',publicRoutes); 
-app.use('/api',apiRoutes); 
-app.all('*', function(req, res, next){
-  res.sendFile(path.join(__dirname+"/public/app/views/index.html"))
+app.use('/', publicRoutes);
+app.use('/api', apiRoutes);
+app.all('*', function(req, res, next) {
+  res.sendFile(path.join(__dirname + "/public/app/views/index.html"))
 })
-app.listen(config.port,"0.0.0.0");
-  
+app.listen(config.port, "0.0.0.0");
+
 /*var server = https.createServer(options, app);
     server.listen(config.port, "0.0.0.0");*/
 
-  /*console.log(server);*/
+/*console.log(server);*/
 /*});*/
 
 /*io.on('connection', function(socket){
 	console.log('a user connected');
 })
 */
-
