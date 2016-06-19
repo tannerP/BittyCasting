4aaab1fe02802acecede1d8f59001e20c63f6ace
Tanner <tannerphan@gmail.com>
2016-06-19 16:51:31 -0700

    Favorites filter fix

--- public/app/filters/applicantFilters.js
+++ public/app/filters/applicantFilters.js
@@ -3,16 +3,20 @@ angular.module('applicantFilters', [])
 .filter('applicants', function() {
       return function(applicants, filter) {
         var filtered = [];
+        console.log(filter)
         switch (filter) {
           case "Favorites":
             if (!applicants) return applicants;
+            console.log(applicants.length)
             for (var a in applicants) {
               var applicant = applicants[a];
-              if (!applicant.favs || applicant.favs.length === 0) return filtered;
+              /*if (!applicant.favs || applicant.favs.length === 0) return filtered;*/
 
               /*for (var f in applicant.favs) {*/
-                var cur = applicant.favs[f]
-                if (applicant.favorited) {
+                /*var cur = applicant.favs[a]*/
+                console.log(applicant.favs.length)
+                if (applicant.favs.length > 0) {
+                  console.log("pushing favs")
                   filtered.push(applicant);
                 }
               /*}*/
